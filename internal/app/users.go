633fcb34a5c11520502682fefdb73b1aced9ade4
Andrey Lipaev <Andrey.Lipayev@gmail.com>
2022-02-05 16:36:18 +0300

    Increment 9 - fix user handler

--- internal/app/users.go
+++ internal/app/users.go
@@ -27,6 +27,7 @@ func (a *application) UserURLs(w http.ResponseWriter, r *http.Request) {
 		u = append(u, UserJSON{ShortURL: fmt.Sprintf("%s/%s", a.cfg.BaseURL, k), OriginalURL: v})
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(u); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
